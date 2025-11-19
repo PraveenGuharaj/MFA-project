5bd18a6f93b4913161c07f9da3ddb3866e156a28
praveenguharaj <praveenrishi3110@gmail.com>
2025-11-19 09:54:45 +0530

    feat: table feature developed

--- src/app/component/mfa-deals/mfa-deals/mfa-deals.ts
+++ src/app/component/mfa-deals/mfa-deals/mfa-deals.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-mfa-deals',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, NgxChartsModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    NgxChartsModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    NgClass
+  ],
   templateUrl: './mfa-deals.html',
   styleUrls: ['./mfa-deals.scss']
 })
 export class MfaDeals {
-
+  activeView: 'table' | 'board' = 'table';
   summary = [
     { icon: 'insert_chart', label: 'Weighted pipeline value', value: '45', color: '#FF9F43' },
     { icon: 'account_balance_wallet', label: 'Forecasted value', value: '56M', color: '#EA4C89' },
@@ -47,6 +66,122 @@ export class MfaDeals {
     { name: 'Low', value: 11, color: '#90A4AE' },
     { name: 'None', value: 2, color: '#E0E0E0' }
   ];
+
+  displayedColumns = [
+    'select',
+    'name',
+    'createdBy',
+    'createdDate',
+    'size',
+    'assignedTo',
+    'stage',
+    'priority',
+    'nextCall'
+  ];
+
+  dataSource = [
+    {
+      name: 'TR Capital',
+      createdBy: { name: 'Devon Lane', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Cody Fisher', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Prospects',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Gillette',
+      createdBy: { name: 'Ronald Richards', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Cody Fisher', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Prospects',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Pizza Hut',
+      createdBy: { name: 'Robert Fox', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Cody Fisher', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Prospects',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Pulivinar vitae',
+      createdBy: { name: 'Jacob Jones', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Ralph Edward', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'POC',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Starbucks',
+      createdBy: { name: 'Guy Hawkins', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Ronald Richar', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Closed',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Louis Vuitton',
+      createdBy: { name: 'Floyd Miles', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Darreil Stewar', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Pricing',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'Louis Vuitton',
+      createdBy: { name: 'Floyd Miles', avatar: 'assets/images/man-user-circle-icon' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Darreil Stewar', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Pricing',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+    {
+      name: 'TR Capital',
+      createdBy: { name: 'Devon Lane', avatar: 'images/man-user-circle-icon.png' },
+      createdDate: '07/12/2022',
+      size: '$120,45,121,565',
+      assignedTo: { name: 'Cody Fisher', avatar: 'images/man-user-circle-icon.png' },
+      stage: 'Prospects',
+      stageColor: '#4FC3F7',
+      priorityColor: '#FFEB3B',
+      nextCall: '07/12/2022'
+    },
+  ];
+
   priorityColors = this.priorityChartData.map(i => ({ name: i.name, value: i.color }));
   priorityTotal = this.priorityChartData.reduce((a, b) => a + b.value, 0);
+  headerName = 'Pipeline';
+
+  getDealsByStage(stage: string) {
+    return this.dataSource.filter(d => d.stage === stage);
+  }
+
+  getStageCount(stage: string) {
+    return this.getDealsByStage(stage).length;
+  }
+  pipelineButton(event: any) {
+    this.headerName = event;
+  }
 }
