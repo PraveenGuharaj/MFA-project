e5c847be47897dcb89b0abc8fb7f2b01e4d39e12
praveenguharaj <praveenrishi3110@gmail.com>
2026-02-04 15:49:26 +0530

    feat: pagination functionality completed.

--- src/app/component/admin-center/admin-center-manage-license/admin-center-manage-license.ts
+++ src/app/component/admin-center/admin-center-manage-license/admin-center-manage-license.ts
@@ -21,6 +21,10 @@ export class AdminCenterManageLicense {
   getLicenseData: any;
   showDeleteConfirm: boolean = false;
   selectedProduct: any;
+  pageSize = 10;
+  currentPage = 1;
+  totalPages = 0;
+  pagedProducts: any[] = [];
   constructor(private adminCenterService: AdminCenterService, public dialog: MatDialog,
     private commonToaster: CommonToaster
   ) { }
@@ -66,6 +70,8 @@ export class AdminCenterManageLicense {
   getLicense() {
     this.adminCenterService.getLicense().subscribe((res: any) => {
       this.getLicenseData = res.data
+      this.totalPages = Math.ceil(this.getLicenseData.length / this.pageSize);
+      this.setPage(1);
     })
   }
 
@@ -106,5 +112,35 @@ export class AdminCenterManageLicense {
       }
 
     })
+  }
+
+
+  setPage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+
+    this.currentPage = page;
+    const start = (page - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    this.pagedProducts = this.getLicenseData.slice(start, end);
+  }
+
+  get pages(): number[] {
+    if (this.totalPages <= 5) {
+      return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    }
+
+    if (this.currentPage <= 3) {
+      return [1, 2, 3];
+    }
+
+    if (this.currentPage >= this.totalPages - 2) {
+      return [this.totalPages - 2, this.totalPages - 1, this.totalPages];
+    }
+
+    return [
+      this.currentPage - 1,
+      this.currentPage,
+      this.currentPage + 1
+    ];
   }
 }
