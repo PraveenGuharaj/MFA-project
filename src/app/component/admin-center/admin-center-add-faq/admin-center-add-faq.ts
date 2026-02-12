85dc4b09424f23dd93d12f85156b595a413c884b
praveenguharaj <praveenrishi3110@gmail.com>
2026-02-12 15:35:29 +0530

    feat: admin center faq create api implemented.

--- src/app/component/admin-center/admin-center-add-faq/admin-center-add-faq.ts
+++ src/app/component/admin-center/admin-center-add-faq/admin-center-add-faq.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AdminCenterService } from '../admin-center-service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CommonToaster } from '../../../shared/services/common-toaster';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-admin-center-add-faq',
@@ -14,7 +18,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatInputModule,
     MatIconModule,
     ReactiveFormsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatCheckboxModule
   ],
   templateUrl: './admin-center-add-faq.html',
   styleUrl: './admin-center-add-faq.scss',
@@ -23,43 +28,126 @@ export class AdminCenterAddFaq {
   productForm!: FormGroup;
 
   // Dropdown Options
-  channels = ["SMS", "Email", "App", "Web"];
-  categories = ["Login", "Transaction", "Reset", "Verification"];
+  channels = ["Mobile banking", "Internet Banking"];
+  categories = ["Login", "Forget Password", "Reset", "Verification"];
   domains = ["Banking", "Insurance", "Fintech", "Wallet"];
   blockDurations = ["30 Sec", "1 Min", "2 Min", "5 Min"];
   types = ["Primary", "Secondary", "Backup"];
   deliveryModesOptions = ["SMS", "Email", "WhatsApp", "IVR"];
   showPassword: boolean = false;
+  getCategoryData: any;
+  getChannelData: any;
+  isEditMode = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private adminCenterService: AdminCenterService,
+    private commonToaster: CommonToaster, private dialogRef: MatDialogRef<AdminCenterAddFaq>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
     this.productForm = this.fb.group({
-      productName: ['', Validators.required],
+      rowNumber: ['', Validators.required],
       channel: ['', Validators.required],
       category: ['', Validators.required],
-
-      // Newly mapped fields
-      otpLength: ['', Validators.required],
-      maxAttempts: ['', Validators.required],
-      otpExpiry: ['', Validators.required],
-      blockDuration: ['', Validators.required],
-      domain: ['', Validators.required],
-      deliveryModes: [[], Validators.required], // Multiple select
-
-      type: ['', Validators.required],
-      status: [true]
+      englishQuestion: ['', Validators.required],
+      englishResponse: ['', Validators.required],
+      addQuestion: ['', Validators.required],
+      ArabicResponse: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      status: ['', Validators.required],
     });
   }
 
-  submitForm() {
-    if (this.productForm.valid) {
-    } else {
-      this.productForm.markAllAsTouched();
-    }
+
+  ngOnInit() {
+    this.getCategoryApi();
+    this.getChannelApi();
   }
+
+  submitForm() {
+    console.log('form', this.productForm.value);
+
+
+    const form = this.productForm.value;
+
+    const payload = {
+      englishQuestion: form.englishQuestion,
+      englishResponse: form.englishResponse,
+      arabicQuestion: form.addQuestion,          // renamed
+      arabicResponse: form.ArabicResponse,       // renamed
+
+      startDate: this.formatDate(form.startDate),
+      endDate: this.formatDate(form.endDate),
+
+      status: form.status ? 'Y' : 'N',
+      faqId: 0,
+
+      channelId: form.channel.map((c: any) => c.channelId),
+
+      channelName: form.channel
+        .map((c: any) => c.name)
+        .join(', '),
+
+      rowNumber: Number(form.rowNumber),
+
+      categoryId: form.category?.categoryId
+    };
+
+    console.log('payload', payload)
+
+
+    if (this.isEditMode) {
+      this.adminCenterService.updateLicense(payload).subscribe((res: any) => {
+        console.log('ressss', res);
+
+        if (res?.status.code == "000000") {
+          this.commonToaster.showSuccess('Product created successfully');
+          this.dialogRef.close('retaiClose');
+        } else {
+
+        }
+      })
+    } else {
+      this.adminCenterService.createFaq(payload).subscribe((res: any) => {
+        console.log('create', res);
+        if (res?.status.code == "000000") {
+          this.commonToaster.showSuccess('Faq created successfully');
+          this.dialogRef.close('retaiClose');
+          this.adminCenterService.trigger();
+        }
+
+      })
+    }
+
+  }
+
+  formatDate(date: string) {
+    const d = new Date(date);
+
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minutes = String(d.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:00`;
+  }
+
 
   closeForm() { }
 
   togglePassword() {
     this.showPassword = !this.showPassword;
+  }
+
+  getCategoryApi() {
+    this.adminCenterService.getCategory().subscribe((res: any) => {
+      this.getCategoryData = res.data;
+    })
+  }
+
+  getChannelApi() {
+    this.adminCenterService.getChannel().subscribe((res: any) => {
+      this.getChannelData = res.data;
+    })
   }
 }
